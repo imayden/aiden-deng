a3aa3826df769925560aad070ceb7664ed7fdd2d
imayden <ayden.yiming.deng@gmail.com>
2024-08-04 15:00:14 -0400

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' }, // Default to 'home'
-  { path: 'home', component: HomeComponent },          
+  { path: '', component: HomeComponent },          
   { path: '**', redirectTo: 'home' }                   // Any undefined path defaults to 'home'
 ];
 
