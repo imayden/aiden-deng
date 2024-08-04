36664f5590e3c6e47cdf15044cd2651d8bec4a88
imayden <ayden.yiming.deng@gmail.com>
2024-08-04 15:57:42 -0400

    update

--- src/app/components/header-bar/header-bar.component.ts
+++ src/app/components/header-bar/header-bar.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-header-bar',
   templateUrl: './header-bar.component.html',
   styleUrl: './header-bar.component.scss'
 })
-export class HeaderBarComponent {
+export class HeaderBarComponent implements OnInit, OnDestroy {
+  today: Date = new Date();
+  yearOfExperience: number = this.today.getFullYear() - 2018;
+  private intervalId: any;
+
+  constructor() { }
+
+  ngOnInit(): void { 
+    this.updateTime();
+    this.intervalId = setInterval(
+      () => {
+        this.updateTime();
+      }, 1000
+    );
+  }
+
+  ngOnDestroy(): void {
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+    }
+
+  }
+
+  updateTime() {
+    this.today = new Date();
+  }
 
 }
