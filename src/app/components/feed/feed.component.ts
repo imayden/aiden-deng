b67dbedd52904d597b814a76f7c78eb690009bff
imayden <ayden.yiming.deng@gmail.com>
2024-08-05 10:58:02 -0400

    update

--- src/app/components/feed/feed.component.ts
+++ src/app/components/feed/feed.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { HeaderBarComponent } from '../header-bar/header-bar.component';
 import { SharedService } from '../../services/shared.service';
+import * as moment from 'moment-timezone';
 
 @Component({
   selector: 'app-feed',
@@ -11,11 +11,19 @@ export class FeedComponent implements OnInit, OnDestroy {
   private intervalId: any;
   yearOfExperience: number = 0;
   today: Date = new Date();
+  currentTime: string = '';
+  currentSeconds: string = '';
+  currentDate: string = '';
+  currentDay: string = '';
+  timeZone: string = ''; // Default time zone
+  timezoneName: string = '';
+  selectedTimezone: 'EST' | 'CST' | 'PST' | 'MST'  = 'EST';
+
 
   constructor(private sharedService: SharedService) { }
 
   ngOnInit(): void { 
-    
+    this.setTimezone(this.selectedTimezone);
     this.updateTime();
     this.intervalId = setInterval(
       () => {
@@ -32,8 +40,32 @@ export class FeedComponent implements OnInit, OnDestroy {
 
   }
 
+  setTimezone(zone: 'EST' | 'CST' | 'PST' | 'MST') {
+    if (zone === 'EST') {
+      this.timeZone = 'America/New_York';
+      this.timezoneName = 'EST';
+    } else if (zone === 'CST') {
+      this.timeZone = 'America/Chicago';
+      this.timezoneName = 'CST';
+    } else if (zone === 'PST') {
+      this.timeZone = 'America/Los_Angeles';
+      this.timezoneName = 'PST';
+    } else if (zone === 'MST') {
+      this.timeZone = 'America/Denver';
+      this.timezoneName = 'MST';
+    }
+  }
+
+
   updateTime() {
     this.today = new Date();
+    
+    const now = moment.default().tz(this.timeZone);
+    this.currentDay = now.format('A'); // AM or PM
+    this.currentDate = now.format('MMM D, YYYY'); // Aug 5, 2024
+    this.currentTime = now.format('h:mm'); // 1:30
+    this.currentSeconds = now.format('ss'); // 30
+    this.timezoneName = now.format('z');
   }
 
 }
